4cadb6122a1fb663ba2e9f1a3189c205a48fcc16
MrFwen <wofengwen@vip.qq.com>
2017-06-16 17:11:00 +0800

    第二次修改

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -0,0 +1,62 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+    /**
+     * 获取extra的值
+     */
+    private boolean mAnswerIsTrue;
+
+
+    //使用静态方法创建Intent
+    public static Intent newIntent(Context packageContext,boolean answerIsTrue){
+        Intent i = new Intent(packageContext,CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue);
+        return i;
+    }
+
+    /**
+     *解析结果intent
+     */
+    public static boolean wasAnswerShown(Intent result){
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN,false);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
+
+        mShowAnswer = (Button)findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue){
+                    mAnswerTextView.setText(R.string.true_button);
+                }else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+            }
+        });
+    }
+    private void setAnswerShownResult(boolean isAnswerShown){
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown);
+        setResult(RESULT_OK,data);
+    }
+}
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,9 +12,19 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    //TAG常量，查看日志信息
+    private static final String TAG = "QuizActivity";
+    //新增键值对保存旋转前的数据
+    private static final String KEY_INDEX = "index";
+    //请求代码常量
+    private static final int REQUEST_CODE_CHEAT = 0;
+
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
+
     private TextView mQusetionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -22,8 +35,11 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_5,true),
     };
     private int mCurrentIndex = 0;
+    //返回处理结果
+    private boolean mIsCheater;
     //封装公共代码
     private void updateQuestion(){
+        Log.d(TAG,"Updating question text for question #" + mCurrentIndex,new Exception());
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQusetionTextView.setText(question);
     }
@@ -34,10 +50,16 @@ public class QuizActivity extends AppCompatActivity {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
         int messageResId = 0;
-        if (userPressedTrue == answerIsTrue){
-            messageResId = R.string.correct_toast;
+
+        if (mIsCheater){
+            messageResId = R.string.judgment_toast;
         }else {
-            messageResId = R.string.incorrect_toast;
+
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
     }
@@ -45,13 +67,13 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called"); //日志输出代码
         setContentView(R.layout.activity_quiz);
-
 
         /**
          * 在启动时就有题目显示在屏幕上（40-41行）
          */
-        mQusetionTextView = (TextView)findViewById(R.id.question_text_view);
+       mQusetionTextView = (TextView)findViewById(R.id.question_text_view);
         updateQuestion();
 
 
@@ -81,9 +103,88 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 mCurrentIndex = (mCurrentIndex +1)% mQuestionBank.length; //序号从1开始，避免越界
+                mIsCheater = false;
                 updateQuestion();
             }
         });
+
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**
+                 * 常用的启动方式
+                 */
+                //Start CheatActivity启动另外一个activity
+                //Intent i = new Intent(QuizActivity.this,CheatActivity.class);
+
+                /**
+                 * 用一个extra启动
+                 */
+
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent i = CheatActivity.newIntent(QuizActivity.this,answerIsTrue);
+                //startActivity(i);
+                startActivityForResult(i,REQUEST_CODE_CHEAT);
+
+            }
+        });
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
         //updateQuestion();
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT){
+            if (data == null){
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
+    }
+
+    /**
+     * 覆盖方法
+     */
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+            Log.i(TAG,"onSaveInstaceState");
+        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
+    /**
+     * 覆盖更多生命周期
+     */
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
     }
 }
